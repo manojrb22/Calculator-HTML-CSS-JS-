ba349715186c942545ca7460e55157fa98e68827
manojrb22 <119850031+manojrb22@users.noreply.github.com>
2024-01-08 11:39:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,18 @@
-const buttons = document.querySelectorAll('button');
-const display = document.getElementById('display');
-let firstOperand;
-let operation;
+document.querySelectorAll('.calculator-keys button').forEach(button => {
+  button.addEventListener('click', function() {
+      const screen = document.querySelector('.calculator-screen');
+      const buttonValue = this.value;
+      const currentScreenValue = screen.value;
+      let newScreenValue;
 
-buttons.forEach(button => {
- button.addEventListener('click', () => {
-    if (button.textContent === 'C') {
-      display.value = '';
-      firstOperand = null;
-      operation = null;
-    } else if (isNaN(parseInt(button.textContent))) {
-      if (operation) {
-        return;
-      }
-      if (!firstOperand) {
-        firstOperand = parseFloat(display.value);
+      if (buttonValue === 'all-clear') {
+          newScreenValue = '';
+      } else if (buttonValue === '=') {
+          newScreenValue = eval(currentScreenValue);
       } else {
-        firstOperand = performOperation(firstOperand, parseFloat(display.value), operation);
+          newScreenValue = currentScreenValue + buttonValue;
       }
-      operation = button.textContent;
-      display.value = '';
-    } else {
-      display.value += button.textContent;
-    }
 
-    if (button.textContent === '=') {
-      if (!firstOperand || !operation) {
-        return;
-      }
-      display.value = performOperation(firstOperand, parseFloat(display.value), operation);
-      firstOperand = null;
-      operation = null;
-    }
- });
+      screen.value = newScreenValue;
+  });
 });
-
-function performOperation(firstOperand, secondOperand, operation) {
- switch (operation) {
-    case '+':
-      return firstOperand + secondOperand;
-    case '-':
-      return firstOperand - secondOperand;
-    case '*':
-      return firstOperand * secondOperand;
-    case '/':
-      return firstOperand / secondOperand;
-    default:
- }}
